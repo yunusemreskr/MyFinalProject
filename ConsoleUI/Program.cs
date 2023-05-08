43de1399f5e822ff69afecd87763b06f38398924
Yunus Emre Seker <mimaryunusemre1@gmail.com>
2023-05-08 18:08:42 +0300

    08.05.2023

--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -1,17 +1,25 @@
 ﻿using Business.Concrete;
+using DataAccess.Concrete.EntityFramework;
 using DataAccess.Concrete.InMemory;
+using Entities.Concrete;
+using System.Collections.Generic;
+using System.Net.Http.Headers;
 
 public class Program
 {
+    //SOLID
+    //Open Closed Principle
     private static void Main(string[] args)
     {
-        ProductManager productManager = new ProductManager(new InMemoryProductDal());
+        ProductManager productManager = new ProductManager(new EfProductDal());
 
-        foreach (var product in productManager.GetAll())
+        foreach (var product in productManager.GetAllByCategoryId(2))
         {
             Console.WriteLine(product.ProductName);
         }
 
         
+
+        //Console.WriteLine(List<Product> GetAll());
     }
 }
