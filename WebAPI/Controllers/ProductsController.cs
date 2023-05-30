cf9cd528c3e637b73324d838a6bee623c456e80c
Yunus Emre Seker <mimaryunusemre1@gmail.com>
2023-05-30 12:57:11 +0300

    30.05.2023

--- WebAPI/Controllers/ProductsController.cs
+++ WebAPI/Controllers/ProductsController.cs
@@ -1,4 +1,7 @@
-﻿using Entities.Concrete;
+﻿using Business.Abstract;
+using Business.Concrete;
+using DataAccess.Concrete.EntityFramework;
+using Entities.Concrete;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
@@ -8,14 +11,22 @@ namespace WebAPI.Controllers
     [ApiController]
     public class ProductsController : ControllerBase
     {
+        //Loosely coupled -
+        //naming convention
+        //IoC Container -- Inversion of Control 
+        IProductService _productService;
+
+        public ProductsController(IProductService productService)
+        {
+            _productService = productService;
+        }
+
         [HttpGet]
         public List<Product> Get() 
         {
-            return new List<Product>
-            {
-                new Product{ProductId=1 , ProductName="Elma"},
-                new Product{ProductId=2 , ProductName="Armut"},
-            };
+            //Dependency chain --             
+            var result = _productService.GetAll();
+            return result.Data;
         }
     }
 }
