1d3bcba0449d97e44235d99cda0ef0f41c78a0d0
Yunus Emre Seker <mimaryunusemre1@gmail.com>
2023-06-09 16:24:12 +0300

    Update

---
+++ Core/Entities/Concrete/UserOperationClaim.cs
@@ -0,0 +1,10 @@
+﻿namespace Core.Entities.Concrete
+{
+    public class UserOperationClaim:IEntity
+    {
+        public int id { get; set; }
+        public int UserId { get; set; }
+        public int OperationClaimId { get; set; }
+
+    }
+}
